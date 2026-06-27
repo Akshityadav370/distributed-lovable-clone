40073757a748d69f59eb78d87e2413f000b40f66
Akshit Yadav <akshit07032001@gmail.com>
2026-06-27 17:32:49 +0530

    init

---
+++ account-service/src/main/java/com/spring_learn/distributed_lovable_clone/account_service/AccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring_learn.distributed_lovable_clone.account_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+}
---
+++ api-gateway/src/main/java/com/spring_learn/distributed_lovable_clone/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.spring_learn.distributed_lovable_clone.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
